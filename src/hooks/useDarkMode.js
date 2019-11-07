013371107f4e4664f9c4b9aa1ad216a84980e90a
Tyree Lebeau <tyreelebeau@Tyrees-iMac.local>
2019-11-06 19:31:27 -0600

    Add useDarkMode functionality, with some comments

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,7 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {useLocalStorage} from "./useLocalStorage.js";
 
-
+// setting state
 export const useDarkMode = () => {
-    
+    const[storedValue, setValue] = useLocalStorage('darkMode', false);
+
+    const body = document.querySelector('body');
+
+    useEffect (() => {
+        //creating an if else statement to give option for darkmode
+        if(storedValue) {
+            body.classList.add('dark-mode');
+        } else {
+            body.classList.remove('dark-mode');
+        }
+    }, [storedValue])
+
+    return [storedValue, setValue];
 }
