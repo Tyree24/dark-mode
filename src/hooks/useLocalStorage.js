ffaadf5307c6b0fbf11179de8f98bc874c20fa48
Tyree Lebeau <tyreelebeau@Tyrees-iMac.local>
2019-11-06 20:16:05 -0600

    finished MVP

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,21 +1,16 @@
 import React, { useState } from "react";
 
-export const useLocalStorage = (key, initialValue) => {
+export const useLocalStorage = ( key, initialValue) => {
 
-const [storedVaule, setStoredValue] = useState (() => {
-// Get item from localStorage by key
-const item = window.localStorage.getItem(key);
-  // If item is in local storage, parse and return stored JSON.
-    // if undefined, return the initialValue
-    return item ? JSON.parse (item) : initialValue;
-}
-)};
+    const item = JSON.parse(localStorage.getItem(key))
 
-const setValue = (value) => {
-    // Set state
-    setStoredValue(value);
-    //Save to local storage 
-    window.localStorage.setItem(key, JSON.stringify(value));
-};
-
-return [storedVaule, setValue];
+    const [storedValue, setStoredValue] = useState(item || initialValue);
+  
+    //define setter function that sets a value to localStorage when called & sets state property to new value that needs to be returned as a string with JSON.stringify(value)
+    const setValue = value => {
+      window.localStorage.setItem(key, JSON.stringify(value)) 
+      setStoredValue(value);
+    }
+  
+    return [storedValue, setValue];
+  }
